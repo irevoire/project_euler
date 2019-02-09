10c8db3f61f55bc06c148637a77b0c4f44a5dd4e
Thomas Campistron <irevoire@hotmail.fr>
2019-02-09 01:23:15 +0100

    summation of prime -- slow

---
+++ summation_of_primes/src/main.rs
@@ -0,0 +1,22 @@
+/*
+ * Summation of primes
+ * Problem 10
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
+mod prime;
+
+fn main() {
+    let limit : u64 = 2000000;
+    let mut sum : u64 = 0;
+
+    for prime in prime::Prime::new() {
+        if prime > limit {
+            break
+        }
+        sum += prime;
+    }
+    println!("sum is {}", sum);
+}
