625d15b055d82ce008902012ea83c439a4948af9
Thomas Campistron <irevoire@hotmail.fr>
2019-02-09 01:06:47 +0100

    largest prime factor buggy

--- largest_prime_factor/src/main.rs
+++ largest_prime_factor/src/main.rs
@@ -3,35 +3,21 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // 
 // What is the largest prime factor of the number 600851475143 ?
-fn gcd(a : u32, b : u32) -> u32 {
-    while b != 0 {
-        let remainder = a % b;
-        a = b;
-        b = remainder;
-    }
-    return a;
-}
+
+mod prime;
 
 fn main() {
-    let number = 13195;
-    let mut iteration = 1;
-    let x_fixed = 2;
-    let size = 2;
-    let mut x = 2;
-    let factor = 1;
+    let number : u64 = 600851475143;
+    let mut largest_prime = 1;
 
-    while (factor == 1) {
-        println!("----   iteration {}   ----", iteration);
-        let mut count = 1;
-        while (count <= size) && (factor <= 1) {
-            x = (x * x + 1) % number;
-            factor = gcd((x - x_fixed), number);
-            println!("count = {}  x = {}  factor = {}", count, x, factor);
-            count += 1;
+    for prime in prime::Prime::new() {
+        if prime > number / 2 {
+            break
         }
-        size = 2 * size;
-        x_fixed = x;
-        iteration = iteration + 1;
+        if number % prime == 0 {
+            println!("----   factor {}   ----", prime);
+            largest_prime = prime;
+        }
     }
-    println!("factor is {}", factor);
+    println!("factor is {}", largest_prime);
 }
