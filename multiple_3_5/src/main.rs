664331ccc53f0aa01e13fe54a3030b7eee8c5a78
irevoire <irevoire@hotmail.fr>
2019-05-31 13:40:56 +0000

    use iterator instead of recursive shit

--- multiple_3_5/src/main.rs
+++ multiple_3_5/src/main.rs
@@ -4,22 +4,14 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 fn main() {
-    let v = multiple(7);
-    println!("{:?}", run(999));
-}
+    let sum: u32 = (0..1000)
+        .filter_map(|n| {
+            if n % 3 == 0 || n % 5 == 0 {
+                return Some(n);
+            }
+            None
+        })
+        .sum();
 
-fn run(n : u32) -> u32 {
-    if n <= 0 {
-        return 0
-    }
-    return multiple(n) + run(n - 1)
-}
-
-fn multiple(n : u32) -> u32 {
-    if n % 3 == 0 {
-        return n
-    } else if n % 5 == 0 {
-        return n
-    }
-    0
+    println!("{}", sum);
 }
