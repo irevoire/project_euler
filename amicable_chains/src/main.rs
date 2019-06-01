14a6d47d5d327a80681954da510dfc6839f4f5be
Thomas Campistron <irevoire@hotmail.fr>
2019-06-01 11:36:06 +0200

    try using memoization... still too slow

--- amicable_chains/src/main.rs
+++ amicable_chains/src/main.rs
@@ -1,4 +1,5 @@
 use rayon::prelude::*;
+use std::collections::HashSet;
 use std::sync::Mutex;
 
 const MAX: u64 = 1_000_000;
@@ -11,31 +12,36 @@ fn sum_of_divisors(n: u64) -> u64 {
             sum += i;
         }
     }
-
     sum
 }
 
 /// itern function for the amicable_chains function
 /// will tail call herself until an element appear twice
 /// Modify the given vector
+/// `done` is a HashSet used to see which numbers were already encontered and
+/// see if we can stop early
 /// return false if a number in the chain is too big
-fn __amicable_chains(n: u64, chain: &mut Vec<u64>) -> bool {
+fn __amicable_chains(n: u64, chain: &mut Vec<u64>, done: &Mutex<HashSet<u64>>) -> bool {
     if n > MAX {
         return false;
     } else if chain.contains(&n) {
         chain.push(n);
         return true;
+    } else if done.lock().unwrap().contains(&n) {
+        return false;
     }
+    done.lock().unwrap().insert(n);
+
     chain.push(n);
-    __amicable_chains(sum_of_divisors(n), chain)
+    __amicable_chains(sum_of_divisors(n), chain, done)
 }
 
 /// generate a chains of amicable number
 /// `n`: the base to use for generating the chain
 /// return a chain BUT `n` may not be part of the chain
-fn amicable_chains(n: u64) -> Option<Vec<u64>> {
+fn amicable_chains(n: u64, done: &Mutex<HashSet<u64>>) -> Option<Vec<u64>> {
     let mut chain = Vec::new();
-    if !__amicable_chains(n, &mut chain) {
+    if !__amicable_chains(n, &mut chain, done) {
         return None;
     }
     // the element you gave (which may not be part of the chain) is now last
@@ -57,11 +63,15 @@ fn amicable_chains(n: u64) -> Option<Vec<u64>> {
 }
 
 fn main() {
+    let done = Mutex::new(HashSet::new());
     let max = Mutex::new(0);
     let chains = Mutex::new(Vec::new());
 
     (1..MAX).into_par_iter().for_each(|n| {
-        let chain = amicable_chains(n);
+        if n % 100_000 == 0 {
+            println!("progression: {}/{}", n, MAX);
+        }
+        let chain = amicable_chains(n, &done);
         if chain.is_none() {
             return;
         }
