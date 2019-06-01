058bd31dd33915b1d3ebfcd01b4b48e41dffeac2
Thomas Campistron <irevoire@hotmail.fr>
2019-06-01 05:13:32 +0200

    fix the bruteforce. Now we use all the permutation of all the combination to guess the password
    '

--- XOR_decryption/src/main.rs
+++ XOR_decryption/src/main.rs
@@ -13,22 +13,28 @@ fn main() {
     let cipher = cipher::init(&cipher);
 
     let mut max = 0;
-    ('!' as u8..='~' as u8).combinations(3).for_each(|pass| {
-        let decipher = cipher::decipher(&cipher, &pass);
-        if decipher.is_none() {
-            return;
-        }
+    ('!' as u8..='~' as u8)
+        .combinations(3)
+        .for_each(|combination| {
+            let len = combination.len();
+            itertools::Permutations::from_vals(combination, len).for_each(|pass| {
+                let decipher = cipher::decipher(&cipher, &pass);
+                if decipher.is_none() {
+                    return;
+                }
 
-        let decipher = decipher.unwrap();
-        let nwords = dict::count_words(&decipher, &dict);
-        if nwords > max {
-            max = nwords;
-            println!(
-                "Using the pass {} for {} english words",
-                std::str::from_utf8(&pass).unwrap(),
-                nwords
-            );
-            println!("{}\n\n", decipher);
-        }
-    });
+                let decipher = decipher.unwrap();
+                let nwords = dict::count_words(&decipher, &dict);
+                let lala = std::str::from_utf8(&pass).unwrap();
+                if nwords > max {
+                    max = nwords;
+                    println!(
+                        "Using the pass {} for {} english words",
+                        std::str::from_utf8(&pass).unwrap(),
+                        nwords
+                    );
+                    println!("{}\n\n", decipher);
+                }
+            })
+        });
 }
