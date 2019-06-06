2b640ad4ad315e737f86d870b009c5bf1a3b8ec8
irevoire <irevoire@hotmail.fr>
2019-06-06 11:34:59 +0000

    remove the initializer

--- coin_sums/src/main.rs
+++ coin_sums/src/main.rs
@@ -1,4 +1,4 @@
-fn _possibilities(amount: i32, coins: &[u32]) -> u32 {
+fn possibilities(amount: i32, coins: &[u32]) -> u32 {
     let mut sum = 0;
     if coins.is_empty() {
         return 1;
@@ -6,15 +6,11 @@ fn _possibilities(amount: i32, coins: &[u32]) -> u32 {
     for (i, &c) in coins.iter().enumerate() {
         match amount - c as i32 {
             0 => sum += 1,
-            n if n > 0 => sum += _possibilities(n, &coins[i..]),
+            n if n > 0 => sum += possibilities(n, &coins[i..]),
             _ => (), // nothing on negative
         }
     }
     sum
-}
-
-fn possibilities(c: u32, coins: &[u32]) -> u32 {
-    _possibilities(c as i32, coins)
 }
 
 fn main() {
