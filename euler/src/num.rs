a72b604e694a136ee4eab503d5753fc4f51fcbc1
irevoire <irevoire@protonmail.ch>
2020-11-23 02:41:56 +0100

    add pandigital_multiples

--- euler/src/num.rs
+++ euler/src/num.rs
@@ -2,6 +2,8 @@ use crate::Divisors;
 
 pub trait Num {
     fn is_abundant(self) -> bool;
+
+    fn split_digits(self) -> Vec<u8>;
 }
 
 macro_rules! impl_num {
@@ -9,6 +11,24 @@ macro_rules! impl_num {
         impl Num for $t {
             fn is_abundant(self) -> bool {
                 self < self.sum_of_divisors()
+            }
+
+            /// split the digits of any number
+            /// ```rust
+            /// use euler::num::Num;
+            /// assert_eq!(&123456_u128.split_digits(), &[1, 2, 3, 4, 5, 6]);
+            /// assert_eq!(&42_u64.split_digits(), &[4, 2]);
+            /// assert_eq!(&2_usize.split_digits(), &[2]);
+            /// ```
+            fn split_digits(mut self) -> Vec<u8> {
+                let mut v = Vec::<u8>::new();
+                while self != 0 {
+                    v.push((self % 10) as u8);
+                    self /= 10;
+                }
+
+                v.reverse();
+                v
             }
         }
     };
---
+++ pandigital_multiples/src/main.rs
@@ -0,0 +1,29 @@
+use euler::num::Num;
+use std::collections::HashSet;
+
+fn main() {
+    let mut base = [0; 9];
+    let mut res = 0;
+
+    // we stop at 99999999 because we don't want to get any direct pandigital number
+    for _ in 0..99999999 {
+        base.iter_mut().enumerate().for_each(|(i, e)| *e += i + 1);
+
+        let mut set = HashSet::new();
+        for digits in base.iter().map(|e| e.split_digits()) {
+            let still_pandigital = digits.iter().all(|d| set.insert(*d));
+            if digits.contains(&0) {
+                break;
+            } else if still_pandigital && set.len() == 9 {
+                res = base[0]; // current number
+                println!("current biggest number {}", res);
+                break;
+            } else if !still_pandigital {
+                break;
+            }
+        }
+    }
+
+    // ugly hardcoded solution because *I know* it'll be only made of the two first multiplication
+    println!("The biggest pandigital number is {}{}", res, res * 2);
+}
